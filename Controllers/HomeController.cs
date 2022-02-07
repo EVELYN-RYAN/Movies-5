e350b4ff8e2207872e8fb8bb8a53c1a04400b430
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2022-02-07 11:30:17 -0700

    push everything

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AddMovieContext SomeContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, AddMovieContext name)
+        {
+            _logger = logger;
+            SomeContext = name;
+        }
+        //view name caller my naming convention
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //view name caller my naming convention
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet] // Identify that it is a get request 
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost] //Identify that this is a post for sending the object to be saved
+        public IActionResult AddMovie(MovieRes mr)
+        {
+            SomeContext.Add(mr);
+            SomeContext.SaveChanges();
+            
+
+            return View("Submited"); //Redirects to the submitted page
+        }
+        public IActionResult MyPodcast()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AddMovie.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Movies.Models
+{
+    public class AddMovieContext : DbContext 
+    {
+        //initiallizing i think... I will definitely be asking questions about this one.
+        public AddMovieContext(DbContextOptions<AddMovieContext> options) : base(options)
+        {
+            //Nothing here
+        }
+        public DbSet<MovieRes> Responses { get; set; } //translation: "create database with table formed in movieres and label it Responses. Get and Set that."
+
+        // only when the model is created will this be called.
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieRes>().HasData(
+                new MovieRes
+                {
+                    MovieID = 1, //seed 1
+                    Category = "Drama",
+                    Title = "Amazing Grace",
+                    Year = 2007,
+                    Director = "Michael Apted",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new MovieRes
+                {
+                    MovieID = 2, //seed 2 etc
+                    Category = "Drama",
+                    Title = "The Professor and the Madman",
+                    Year = 2019,
+                    Director = "Farhad Safinia",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                 new MovieRes
+                 {
+                     MovieID = 3,
+                     Category = "Action/Adventure",
+                     Title = "Star Wars Episode III: Revenge of the Sith",
+                     Year = 2005,
+                     Director = "George Lucas",
+                     Rating = "PG-13",
+                     Edited = false,
+                     LentTo = "",
+                     Notes = "Leaves a massive gap"
+                 }
+
+                );
+
+        }
+    }
+}
